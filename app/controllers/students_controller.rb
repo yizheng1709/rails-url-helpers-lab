2b66b28ce6d05269d46a242f2d653a60cc240372
Eva <yi.zheng1709@gmail.com>
2021-03-05 03:52:38 -0500

    commit tree f804d8596b5637f293178387e54fa7dd7ae9e632

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -5,7 +6,16 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def activate
+    @student = Student.find_by(id: params[:id]) 
+    @student.toggle(:active)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   def show
+    # binding.pry
+    @student = Student.find_by(id: params[:id])
   end
 
   private
